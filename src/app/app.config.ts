a47edcf38d146966789bc6e4bae0f894b027df52
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-19 02:05:57 +0200

    Add auth reducer

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,16 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideStore } from '@ngrx/store';
+import { provideState, provideStore } from '@ngrx/store';
 
 import { routes } from './app.routes';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { authFeatureKey, authReducer } from './auth/store/redusers';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideStore(),
+    provideState(authFeatureKey, authReducer),
     provideStoreDevtools({
       maxAge: 25,
       logOnly: !isDevMode(),
